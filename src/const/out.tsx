f8c1b1460a2f84488238c6ae757e7cf80caf87a2
Tanxiao_Orz_DeskTop <13671985248@163.com>
2023-11-19 22:26:26 +0800

    通用out类型抽离

---
+++ src/const/out.tsx
@@ -0,0 +1,37 @@
+export type ModuleOut = {
+    moduleTypeId:number
+    moduleTypeName:string
+    workflowRemark:string
+    creatorId:number
+    creatorName:string
+    createTime:string
+    tableCounts:number
+    flowCounts:number
+    searchCounts:number
+    chartsCounts:number
+};
+
+export type DropSelect = {
+    key:number
+    label:string
+    disable:boolean
+    children:DropSelect[]
+}
+
+export type TableOut = {
+    tableId:number
+    tableViewName:string
+    tableDataName:string
+    moduleNo:number
+    workFlowNo:string
+    detailNames:string[]
+    detailSelect:DropSelect[]
+    groupNames:string[]
+    groupSelect:DropSelect[]
+    remark:string
+    virtual:boolean
+    creator:number
+    createName:string
+    createTime:string
+    moduleName:string
+}
