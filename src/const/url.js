ca789a206aba7f9d1fefd5d2825caafd63b1a378
Tanxiao <13671985248@163.com>
2023-11-21 16:21:58 +0800

    增加更多url路径

--- src/const/url.js
+++ src/const/url.js
@@ -1,6 +1,8 @@
 ﻿export default {
     backUrl:{
-        module:"/back_module"
+        module:"/back_module",
+        table:'/back_table',
+        table_concrete:'/back_table/{id}'
     },
     frontUrl:{
         humanResource:"/human_resource/"
