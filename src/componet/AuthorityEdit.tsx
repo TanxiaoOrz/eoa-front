e7b49192108bfb23bc9a9e161c0a2c7392975144
Tanxiao <13671985248@163.com>
2023-11-27 17:55:02 +0800

    角色权限组件编辑暂存

--- src/componet/AuthorityEdit.tsx
+++ src/componet/AuthorityEdit.tsx
@@ -3,13 +3,28 @@ import { ColumnOut } from "../const/out";
 import { getDataList } from "../const/http";
 import url from "../const/url";
 import { Tabs } from "antd";
-import { ProForm, ProFormDigit, ProFormSelect } from "@ant-design/pro-components";
+import { ProColumns, ProForm, ProFormDigit, ProFormSelect, ProTable } from "@ant-design/pro-components";
 import columnType from "../const/columnType";
 
 type All = {
     start:number
     end:number
 }
+
+type Create = {
+    self:boolean
+    leader:boolean
+    leaderRecursion:boolean
+    depart:boolean
+    section:boolean
+    sectionRecursive:boolean
+}
+
+type Character = {
+    characterId:number
+    grade:number
+}
+
 type Authority = {
     bodyType:string,
     body:Map<string,string>,
@@ -111,7 +126,7 @@ AllConstraint.defaultProps = {
 
 
 const CreatorConstraint = (prop:{
-    default:All|undefined,
+    default:Create|undefined,
     update:(arg0: string) => boolean
 }) => {
     const items = [
@@ -143,31 +158,31 @@ const CreatorConstraint = (prop:{
                 label="直属领导"
                 name="leader"
                 options={items}
-                tooltip="创建人领导是否拥有该权限按"
+                tooltip="创建人领导是否拥有该权限"
             />
             <ProFormSelect
                 label="上级领导"
                 name="leaderRecursion"
                 options={items}
-                tooltip="创建人领导及其领导是否拥有该权限按"
+                tooltip="创建人领导及其领导是否拥有该权限"
             />
             <ProFormSelect
                 label="同部门"
                 name="depart"
                 options={items}
-                tooltip="创建人同部门是否拥有该权限按"
+                tooltip="创建人同部门是否拥有该权限"
             />
             <ProFormSelect
                 label="同分部"
                 name="section"
                 options={items}
-                tooltip="创建人同分布是否拥有该权限按"
+                tooltip="创建人同分布是否拥有该权限"
             />
             <ProFormSelect
                 label="同分部"
                 name="section"
                 options={items}
-                tooltip="创建人分布及其上级分部是否拥有该权限按"
+                tooltip="创建人分布及其上级分部是否拥有该权限"
             />
         </ProForm>)
         }
@@ -180,11 +195,90 @@ const CreatorConstraint = (prop:{
     )
 }
 
-CreatorConstraint.defaultProps = {
+
+
+const CharacterConstraint = (prop:{
+    default:Character|undefined,
+    form:Character|undefined,
+    useForm:boolean,
+    update:(arg0: string) => boolean,
+    updateForm:(arg0: string) => boolean,
+    tableId:number,
+    isVirtual:boolean}) => {
+    const columns:ProColumns<Character>[] = [
+        {
+            key:"name",
+            title:"角色名称",
+            dataIndex:"characterId",
+            request:async () =>{
+                return getDataList()
+            }
+        }
+    ]
+    const alls = [{
+        key:"default",
+        label:"默认",
+        children:(
+        <ProTable
+            
+        )
+        }
+    ]
+    if (prop.useForm)
+        alls.push({
+            key:"form",
+            label:"表单选择",
+            children:(
+            <ProForm 
+                initialValues={prop.form}
+                onFinish={async (values)=>{
+                    return prop.updateForm(JSON.stringify(values))
+                }}    
+            >
+                <ProFormSelect
+                    label="最小安全等级"
+                    name="start"
+                    request={async ()=>{
+                        let columns:ColumnOut[] = (await getDataList(url.backUrl.column,{isVirtual:prop.isVirtual,tableNo:prop.tableId})).data
+                        return columns.filter((column)=>{return column.columnType===columnType.number})
+                            .map((value,index,array)=>{
+                                return {
+                                    value:value.columnId,
+                                    label:value.columnDataName
+                                }
+                            })
+                    }}
+                    placeholder="请选择代表最小安全等级的字段"
+                />
+                <ProFormSelect
+                    label="最大安全等级"
+                    name="end"
+                    request={async ()=>{
+                        let columns:ColumnOut[] = (await getDataList(url.backUrl.column,{isVirtual:prop.isVirtual,tableNo:prop.tableId})).data
+                        return columns.filter((column)=>{return column.columnType===columnType.number})
+                            .map((value,index,array)=>{
+                                return {
+                                    value:value.columnId,
+                                    label:value.columnDataName
+                                }
+                            })
+                    }}
+                    placeholder="请选择代表最大安全等级的字段"
+                />
+            </ProForm>)
+        })
+    return (
+    <Tabs
+        activeKey="default"
+        centered
+        items={alls}></Tabs>
+    )
+}
+
+CharacterConstraint.defaultProps = {
     tableId:0,
     isVirtual:false
 }
-
 
 
 
