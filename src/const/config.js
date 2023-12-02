d403034510c863e555bbd536ef66fac207b8bcde
Tanxiao_Orz_DeskTop <13671985248@163.com>
2023-12-02 21:12:45 +0800

    修改authority的url错误,添加目录模块的url

--- src/const/config.js
+++ src/const/config.js
@@ -9,13 +9,16 @@ export default {
     backs:{
         table : "api/v1/table/back/table",
         module : "api/v1/table/back/module",
-        column : "api/v1/table/back/column"
+        column : "api/v1/table/back/column",
+        content : "api/v1/content/back/content"
     },
     fronts:{
-        character: "/api/v1/front/authority/character",
-        authority: "/api/v1/front/authority/authority",
+        character: "/api/v1/authority/front/character",
+        authority: "/api/v1/authority/front/authority",
         human:"/api/v1/organization/front/human",
         depart:"/api/v1/organization/front/depart",
-        section:"/api/v1/organization/front/section"
+        section:"/api/v1/organization/front/section",
+        content:"/api/v1/content/front/content",
+        file:"/api/v1/content/front/file"
     }
 }
