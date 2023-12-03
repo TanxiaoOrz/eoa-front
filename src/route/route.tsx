93c0ae43b86151966b31264213c3f06b5d4d58fe
Tanxiao_Orz_DeskTop <13671985248@163.com>
2023-12-03 22:44:12 +0800

    route 分为两部分

--- src/route/route.tsx
+++ src/route/route.tsx
@@ -11,34 +11,48 @@ import { FrontPage } from "./fornt.tsx";
 import BackTable from "../pages/TableModule/back_table.tsx";
 import BackTableConcrete from "../pages/TableModule/back_table_concrete.tsx";
 import BackContent from "../pages/ContentModule/back_content.tsx";
+import BackContentConcrete from "../pages/ContentModule/back_content_concrete.tsx";
+import FrontContentConcrete from "../pages/ContentModule/front_content.tsx";
 
 const Protable = protable
 
-const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Root />,
-      errorElement: <ErrorPage />,
-    },{
-      path: "back_module",
-      element: <BackModule />,  
-    },{
-      path:"back_table",
-      element: <BackTable />,
-    },{
-      path:"back_table/:tableId",
-      element: <BackTableConcrete />,
-    },{
-      path: "login",
-      element: <Login />
-    },{
-      path: "front",
-      element: <FrontPage />
-    },{
-      path:"back_content",
-      element: <BackContent />
-    }
+const backs = [
+  {
+    path: "back_module",
+    element: <BackModule />,  
+  },{
+    path:"back_table",
+    element: <BackTable />,
+  },{
+    path:"back_table/:tableId",
+    element: <BackTableConcrete />,
+  },{
+    path:"back_content",
+    element: <BackContent />
+  },{
+    path:"back_content/:contentId",
+    element:<BackContentConcrete />
+  }
+]
 
-]);
+const fronts = [
+  {
+    path: "/",
+    element: <Root />,
+    errorElement: <ErrorPage />,
+  },{
+    path: "login",
+    element: <Login />
+  },{
+    path: "front",
+    element: <FrontPage />
+  },{
+    path:"content/:contentId",
+    element: <FrontContentConcrete />
+  }
+]
+
+
+const router = createBrowserRouter(fronts.concat(backs));
 
 export default router
