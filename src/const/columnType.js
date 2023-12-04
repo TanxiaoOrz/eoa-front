92c80d24e1f1e3d934c13d1792550786ed83b2a4
Tanxiao <13671985248@163.com>
2023-12-04 19:27:39 +0800

    暂存

--- src/const/columnType.js
+++ src/const/columnType.js
@@ -1,9 +1,34 @@
-export default {
+export const columnType = {
     singleText: "SINGLE_TEXT",
     areaText:"TEXT",
     number:"NUMBER",
     browser:"BROWSER_BOX",
-    select:"int",
+    select:"SELECT_ITEM",
     date:"DATETIME",
     file:"FILE"
 }
+
+export const columnTypeSelect = [
+    {
+        label:"单行文本",
+        key:"SINGLE_TEXT"
+    },{
+        label:"多行文本",
+        key:"TEXT"
+    },{
+        label:"数字",
+        key:"NUMBER"
+    },{
+        label:"浏览框",
+        key:"BROWSER_BOX"
+    },{
+        label:"选择框",
+        key:"SELECT_ITEM"
+    },{
+        label:"时间",
+        key:"DATETIME"
+    },{
+        label:"文件",
+        key:"FILE"
+    },
+]
