7382e29a33858eace927dd88e662330158ae3f8a
Tanxiao <13671985248@163.com>
2023-12-21 17:39:31 +0800

    完成humanBack相关页面

--- src/pages/OrganizationModule/back_human.tsx
+++ src/pages/OrganizationModule/back_human.tsx
@@ -1,1 +1,302 @@
-﻿
+﻿import { FolderOpenTwoTone, PlusOutlined } from '@ant-design/icons';
+import { ActionType, ModalForm, ProColumns, ProFormText, ProFormTextArea, ProFormTreeSelect, ProTable } from '@ant-design/pro-components';
+import { Button, Dropdown, Form, Layout, MenuProps, Typography } from 'antd';
+import React, { useRef } from 'react';
+import url from '../../const/url.js';
+import { UpdateData, deleteData, getDataList, newData } from '../../const/http.tsx';
+import { Content, Header } from 'antd/es/layout/layout';
+import { CharacterOut, DepartOut, HumanOut, ModuleOut } from '../../const/out.tsx';
+import config from '../../const/config.js';
+
+const { Title } = Typography
+
+type HumanIn = {
+    loginName: string
+    password: string
+    name: string
+    sex: number
+    birth: string
+    age: number
+    telephone: string
+    mail: string
+    phone: string
+    fax: string
+    workCode: string
+    depart: number
+    job: string
+    directorLeader: number
+    supporter: number
+    photo: number
+    signature: string
+    lastLogin: Date
+    safety: number
+}
+
+const CreaterHuman = (prop: { action: React.MutableRefObject<ActionType | undefined> }) => {
+    const [form] = Form.useForm<HumanIn>();
+    let jump = false
+    return (
+        <ModalForm<HumanIn>
+            title="新建人员"
+            trigger={
+                <Button type="primary">
+                    <PlusOutlined />
+                    新建人员
+                </Button>
+            }
+            width={400}
+            form={form}
+            submitter={{
+                searchConfig: {
+                    submitText: '新建',
+                    resetText: '取消',
+                },
+                render: (prop, defaultDoms) => {
+                    return [
+                        ...defaultDoms,
+                        <Button
+                            key="jump"
+                            type='primary'
+                            onClick={() => {
+                                jump = true
+                                prop.submit()
+                            }}>新建并跳转</Button>
+                    ]
+                }
+            }}
+            submitTimeout={2000}
+            autoFocusFirstInput
+            onFinish={async (values: HumanIn) => {
+                let dataId = await newData(config.backs.human, values)
+                if (dataId != -1) {
+                    if (jump)
+                        window.location.assign(url.backUrl.human_concrete+dataId)
+                    if (prop.action.current !== undefined)
+                                prop.action.current.reload();
+                }
+                return dataId != -1
+            }}
+            modalProps={{
+                destroyOnClose: true,
+                onCancel: () => console.log('run'),
+            }}>
+            <ProFormText
+                width="md"
+                name="loginName"
+                label="登录名"
+                tooltip="最长为33位"
+                placeholder="请输入角色名称"
+                required={true} />
+            <ProFormText.Password
+                width="md"
+                name="password"
+                label="预设密码"
+                tooltip="最长33位"
+                placeholder="请输入预设密码"
+                required={true} />
+            <ProFormText
+                width="md"
+                name="name"
+                label="姓名"
+                tooltip="最长为33位"
+                placeholder="请输入姓名"
+                required={true} />
+            <ProFormText
+                width="md"
+                name="workCode"
+                label="工号"
+                tooltip="最长为33位"
+                placeholder="请输入工号"
+                required={true} />
+            <ProFormTreeSelect
+                width="md"
+                name="depart"
+                label="所属部门"
+                tooltip="必须为非废弃部门"
+                placeholder="请选择部门"
+                required={true} 
+                request={async ()=>{
+                    let departs:DepartOut[] = (await getDataList(config.fronts.depart,{toBrowser:true,isDeperacted:0})).data
+                    return departs.map((depart,index,array)=>{return {title:depart.departName,value:depart.dataId}})
+                }}/>
+        </ModalForm>
+
+    )
+}
+
+
+const HumanList = (prop:{depart:number,section:number}) => {
+    const actionRef = useRef<ActionType>();
+
+    const columns: ProColumns<HumanOut>[] = [
+        {
+            key: 'code',
+            title: '编号',
+            dataIndex: 'dataId',
+            valueType: "indexBorder",
+            width: 48,
+            align: "center"
+
+        },
+        {
+            key: 'name',
+            title: '人员姓名',
+            dataIndex: 'name',
+        },
+        {
+            key: 'sex',
+            title: '人员性别',
+            dataIndex: 'sex',
+            ellipsis: true,
+            valueType:'select',
+            tip: "不显示代表未录入",
+            request:async ()=>{
+                return [
+                    {
+                        label:"男",
+                        value:0,
+                    },{
+                        label:"女",
+                        value:1,
+                    },
+                ]
+            }
+        },
+        {
+            key: 'telephone',
+            title: '联系电话',
+            dataIndex: 'telephone',
+        },
+        {
+            key: 'workCode',
+            title: '工号',
+            dataIndex: 'workCode',
+        },
+        {
+            key: 'depart',
+            title: '所属部门',
+            dataIndex: 'depart',
+            valueType: "select",
+            request: async ()=>{
+                let departs:DepartOut[] = (await getDataList(config.fronts.depart,{toBrowser:true,isDeperacted:0})).data
+                return departs.map((depart,index,array)=>{return {label:depart.departName,value:depart.dataId}})
+            }
+        },{
+            key:'isDeprecated',
+            dataIndex:'isDeprecated',
+            valueType:'select',
+            request:async ()=>{
+                return [
+                    {
+                        label:'在职',
+                        value:0
+                    },{
+                        label:'离职',
+                        value:1
+                    },
+                ]
+            }
+        },{
+            key: 'action',
+            title: '操作',
+            dataIndex: "moduleTypeId",
+            width: 48 * 3,
+            hideInSearch: true,
+            render: (dom, entity, index, action) =>
+                <Button href={url.backUrl.human_concrete + entity.dataId}>编辑</Button>
+        },
+    ];
+
+    return (
+        <ProTable<HumanOut>
+            columns={columns}
+            actionRef={actionRef}
+            cardBordered
+            request={async (
+                // 第一个参数 params 查询表单和 params 参数的结合
+                // 第一个参数中一定会有 pageSize 和  current ，这两个参数是 antd 的规范
+                params,
+                sort,
+                filter,
+            ) => {
+                // 这里需要返回一个 Promise,在返回之前你可以进行数据转化
+                // 如果需要转化参数可以在这里进行修改
+                // console.log(params)
+                // console.log(sort);
+                // console.log(filter);
+                if (prop.depart !== 0)
+                    params.depart = prop.depart
+                if (prop.section !== 0)
+                    params.section = prop.section
+                return getDataList(config.backs.human, params)
+            }}
+            editable={{
+                type: 'multiple',
+            }}
+            columnsState={{
+                persistenceKey: 'pro-table-singe-demos',
+                persistenceType: 'localStorage',
+                onChange(value) {
+                    console.log('value: ', value);
+                },
+            }}
+            rowKey="id"
+            search={{
+                labelWidth: 'auto',
+            }}
+            options={{
+                setting: {
+                    listsHeight: 400,
+                },
+            }}
+            form={{
+                // 由于配置了 transform，提交的参与与定义的不同这里需要转化一下
+                syncToUrl: (values, type) => {
+                    if (type === 'get') {
+                        return {
+                            ...values,
+                            created_at: [values.startTime, values.endTime],
+                        };
+                    }
+                    return values;
+                },
+            }}
+            pagination={{
+                pageSize: 10,
+                onChange: (page) => console.log(page),
+            }}
+            dateFormatter="string"
+            headerTitle="应用列表"
+            toolBarRender={() => [
+                <CreaterHuman key="create" action={actionRef} />,
+            ]}
+        />
+    );
+};
+
+const BackHuman = (prop:{depart:number,section:number}) => {
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
+            <Header style={{ display: 'flex', alignItems: 'center', background: "#ffffff", borderRadius: "8px", }}>
+                <div style={{ display: 'flex' }}>
+                    <FolderOpenTwoTone style={{ fontSize: "36px", marginTop: "15px", marginLeft: "5px" }} />
+                    <Title level={2} style={{ color: 'GrayText', marginLeft: '10px', marginBottom: '15px' }}>人员列表</Title>
+                </div>
+            </Header>
+            <Content style={{ padding: '15px 50px', minHeight: '100%' }}>
+                <HumanList depart={prop.depart} section={prop.section}/>
+            </Content>
+
+        </Layout>
+
+
+
+    )
+}
+
+BackHuman.defaultProps = {
+    depart:0,
+    section:0
+}
+
+export default BackHuman;
--- src/pages/OrganizationModule/back_human_concrete.tsx
+++ src/pages/OrganizationModule/back_human_concrete.tsx
@@ -1,1 +1,300 @@
-﻿
+﻿import React, { useEffect, useRef, useState } from "react"
+import config from "../../const/config.js"
+import { UpdateData, deleteData, getDataList, getDataOne } from "../../const/http.tsx"
+import { CharacterOut, Authority, HumanOut, DepartOut, SectionOut } from "../../const/out.tsx"
+import { useParams } from "react-router"
+import url from "../../const/url"
+import PageWait from "../../componet/PageWait.tsx"
+import { ActionType, ModalForm, PageContainer, ProColumns, ProForm, ProFormDatePicker, ProFormDigit, ProFormGroup, ProFormSelect, ProFormText, ProFormTextArea, ProFormTreeSelect, ProTable } from "@ant-design/pro-components"
+import { Button, Form, Modal } from "antd"
+import { PlusOutlined } from "@ant-design/icons"
+import UpLoadFile, { UploadFileProposed } from "../../componet/UpLoadFile.tsx"
+
+
+type HumanIn = {
+    loginName: string
+    password: string
+    name: string
+    sex: number
+    birth: string
+    age: number
+    telephone: string
+    mail: string
+    phone: string
+    fax: string
+    workCode: string
+    depart: number
+    job: string
+    directorLeader: number
+    supporter: number
+    photo: number
+    signature: string
+    lastLogin: Date
+    safety: number
+}
+
+
+
+const BaseHumanConcrete = () => {
+    const humanId = useParams().dataId
+    const [form] = Form.useForm<HumanIn>()
+    const [human, setHuman] = useState<HumanOut>()
+    useEffect(() => {
+        if (human === undefined)
+            getDataOne(config.backs.human + "/" + humanId).then((value) => {
+                if (value.success)
+                    setHuman(value.data)
+            })
+    })
+    if (humanId === undefined) {
+        window.location.replace(url.backUrl.human)
+        return (<div></div>)
+    }
+    if (human === undefined)
+        return (<PageWait />)
+    const dropHuman = async () => {
+        return (await deleteData(config.backs.human + "/" + humanId))
+    }
+
+    const HumanBase = () => {
+        return (
+            <div style={{ height: "85vh", display: "flex", background: "#fdfdfd", paddingTop: "30px" }}>
+                <ProForm<HumanIn>
+                    style={{
+                        margin: "0 auto"
+                    }}
+                    submitter={{
+                        searchConfig: {
+                            resetText: "重置",
+                            submitText: "保存"
+                        },
+                        submitButtonProps: {
+                            style: { marginRight: "auto" }
+                        },
+                        resetButtonProps: {
+                            style: { marginLeft: "auto" }
+                        }
+                    }}
+                    layout="horizontal"
+                    initialValues={human}
+                    onFinish={async (human: HumanIn) => {
+                        return (await UpdateData(config.backs.human + "/" + human, human))
+                    }}
+                >
+                    <ProFormGroup size={"large"} title="登录信息">
+                        <ProFormText
+                            width="xl"
+                            name="loginName"
+                            label="登录名"
+                            tooltip="最长为33位"
+                            placeholder="请输入表单显示名称"
+                            required={true}
+                        />
+                        <ProFormText.Password
+                            width="xl"
+                            name="password"
+                            label="密码"
+                            tooltip="最长为33位"
+                            placeholder="不填写即为不修改"
+                            required={false}
+                        />
+                    </ProFormGroup>
+                    <ProFormGroup size={"large"} title="基本信息">
+                        <ProFormText
+                            width="xl"
+                            name="name"
+                            label="姓名"
+                            tooltip="最长为33位"
+                            placeholder="请输入姓名"
+                            required={true}
+                        />
+                        <ProFormSelect
+                            width="xl"
+                            name="sex"
+                            label="性别"
+                            tooltip="最长为33位"
+                            request={async()=>[
+                                {
+                                    label:"男",
+                                    value:0,
+                                },{
+                                    label:"女",
+                                    value:1,
+                                },
+                            ]}
+                            required={false}
+                        />
+                        <ProFormDigit
+                            width="xl"
+                            name="age"
+                            label="年龄"
+                            tooltip="最长为33位"
+                            disabled
+                            fieldProps={{ precision: 0 }}
+                            required={false}
+                        />
+                        <ProFormDatePicker
+                            width="xl"
+                            name="birth"
+                            label="出生日期"
+                            required={false}
+                        />
+                    </ProFormGroup>
+                    <ProFormGroup size="large" title="通讯信息">
+                        <ProFormText
+                            width="xl"
+                            name="telephone"
+                            label="手机号"
+                            tooltip="最长为33位"
+                            placeholder="请输入手机号"
+                            required={false}
+                        />
+                        <ProFormText
+                            width="xl"
+                            name="mail"
+                            label="邮箱"
+                            tooltip="最长为33位"
+                            placeholder="请输入邮箱"
+                            required={false}
+                        />
+                        <ProFormText
+                            width="xl"
+                            name="phone"
+                            label="电话"
+                            tooltip="最长为33位"
+                            placeholder="请输入电话"
+                            required={false}
+                        />
+                        <ProFormText
+                            width="xl"
+                            name="fax"
+                            label="传真"
+                            tooltip="最长为33位"
+                            placeholder="请输入传真"
+                            required={false}
+                        />
+                    </ProFormGroup>
+                    <ProFormGroup size={"large"} title="工作信息">
+                        <ProFormText
+                            width="xl"
+                            name="workCode"
+                            label="工号"
+                            tooltip="最长为33位"
+                            placeholder="请输入工号"
+                            required={true}
+                        />
+                        <ProFormText
+                            width="xl"
+                            name="job"
+                            label="岗位"
+                            tooltip="最长为33位"
+                            placeholder="请输入岗位"
+                            required={false}
+                        />
+                        <ProFormTreeSelect
+                            width="xl"
+                            name="depart"
+                            label="所属部门"
+                            tooltip="仅为未废弃部门"
+                            placeholder="请选择部门"
+                            required={true}
+                            request={async ()=>{
+                                let departs:DepartOut[] = (await getDataList(config.fronts.depart,{toBrowser:true,isDeperacted:0})).data
+                                return departs.map((depart,index,array)=>{return {title:depart.departName,value:depart.dataId}})
+                            }}
+                        />
+                        <ProFormTreeSelect
+                            width="xl"
+                            name="section"
+                            label="所属分部"
+                            tooltip="仅为未废弃分部"
+                            placeholder="请选择分部"
+                            readonly
+                            request={async ()=>{
+                                let sections:SectionOut[] = (await getDataList(config.fronts.depart,{toBrowser:true,isDeperacted:0})).data
+                                return sections.map((depart,index,array)=>{return {title:depart.sectionName,value:depart.dataId}})
+                            }}
+                        />
+                    </ProFormGroup>
+                    <ProFormGroup size={"large"} title="上下级信息">
+                    <ProFormTreeSelect
+                        width="xl"
+                        name="directorLeader"
+                        label="上级领导"
+                        placeholder="请选择领导"
+                        request={async () => {
+                            let humans:HumanOut[] =(await getDataList(config.fronts.human,{toBrowser:true,isDeprecated:0})).data
+                            return humans.map((value,index,array)=>{return {title:value.name,value:value.dataId}})
+                        }}
+                        required = {true}/>
+                    <ProFormTreeSelect
+                        width="xl"
+                        name="supporter"
+                        label="助理"
+                        placeholder="请选择助理"
+                        request={async () => {
+                            let humans:HumanOut[] =(await getDataList(config.fronts.human,{toBrowser:true,isDeprecated:0})).data
+                            return humans.map((value,index,array)=>{return {title:value.name,value:value.dataId}})
+                        }}
+                        required = {true}/>
+                    </ProFormGroup>
+                    <ProFormGroup size={"large"} title="展示资料" >
+                        <UploadFileProposed label="展示照片" name="photo" form={form} content={2}/>
+                        <ProFormTextArea label="个性签名" name="signature" width="xl" placeholder={"请输入签名"}/>
+                    </ProFormGroup>
+                    <ProFormGroup size={"large"} title="上下级信息"> 
+                        <ProFormDatePicker
+                            name={"lastLogin"}
+                            label={"上次登录时间"}
+                            readonly
+                            width={'xl'}
+                        />
+                    </ProFormGroup>
+                </ProForm>
+            </div>
+        )
+    }
+
+    return (
+        <div
+            style={{ background: '#F5F7FA' }}
+        >
+            <PageContainer
+
+                fixedHeader
+                header={{
+                    title: human.name,
+                    breadcrumb: {
+                        items: [
+                            {
+                                path: url.backUrl.human,
+                                title: '角色列表',
+                            },
+                            {
+                                path: url.backUrl.character_concrete + '/' + humanId,
+                                title: human.name,
+                            },
+                        ],
+                    },
+                    extra: [
+                        <Button key='save' type="primary" onClick={form.submit}>{human.isDeprecated===1?"编辑":"编辑并复职"}</Button>,
+                        <Button key='reset' onClick={()=>{form.resetFields()}}>重置</Button>,
+                        <Button key='drop' type='default' danger onClick={dropHuman} disabled={human.isDeprecated===1} >离职</Button>
+                    ]
+                }}
+                tabList={[
+                    {
+                        tab: '基本信息',
+                        key: '1',
+                        children: (<HumanBase />)
+                    }
+                ]}
+            >
+            </PageContainer>
+        </div>
+    )
+}
+
+export default BaseHumanConcrete
+
+
