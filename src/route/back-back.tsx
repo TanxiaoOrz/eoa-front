5317b0511dfb9c312937b2ac1855bc4d7cd53a7e
Tanxiao <13671985248@163.com>
2024-01-20 14:53:49 +0800

    暂存待测试

---
+++ src/route/back-back.tsx
@@ -0,0 +1,458 @@
+﻿import React, { useEffect, useState } from 'react';
+import { AppstoreTwoTone, BankTwoTone, LaptopOutlined, NotificationOutlined, PlusCircleTwoTone, PoweroffOutlined, RetweetOutlined, UserOutlined } from '@ant-design/icons';
+import type { App, MenuProps } from 'antd';
+import { Avatar, Breadcrumb, Button, Divider, Dropdown, Flex, Layout, Menu, Space, theme, Typography } from 'antd';
+import { FileOut, HumanOut, MenuOut, PageConfigOut } from '../const/out.tsx';
+import config from '../const/config';
+import { getDataList, getDataOne } from '../const/http.tsx';
+import PageWait from '../componet/PageWait.tsx';
+import url from '../const/url.js';
+
+
+const { Header, Content, Sider } = Layout;
+const { Title } = Typography
+
+type PageConfig = {
+    companyName: string
+    headerColor: string
+    sideColor: string
+}
+
+const getRootMenuItem = (): MenuProps['items'] => {
+    return [
+        {
+            key: 1,
+            label: <a href={url.back + "?menuId=" + 1}>组织结构</a>
+        }, {
+            key: 2,
+            label: <a href={url.back + "?menuId=" + 2}>权限管理</a>
+        }, {
+            key: 3,
+            label: <a href={url.back + "?menuId=" + 3}>表单设置</a>
+        }, {
+            key: 4,
+            label: <a href={url.back + "?menuId=" + 4}>知识目录</a>
+        }, {
+            key: 5,
+            label: <a href={url.back + "?menuId=" + 5}>工作流程</a>
+        }, {
+            key: 6,
+            label: <a href={url.back + "?menuId=" + 6}>数据展示</a>
+        }, {
+            key: 7,
+            label: <a href={url.back + "?menuId=" + 7}>页面菜单</a>
+        },
+    ]
+}
+
+const getFirstPage = (menuId: string | null): string => {
+    switch (parseInt(menuId ?? "1")) {
+        case 7:
+            return url.backUrl.menu
+        case 6:
+            return url.backUrl.search_list
+        case 5:
+            return url.backUrl.workflow
+        case 4:
+            return url.backUrl.content
+        case 3:
+            return url.backUrl.module
+        case 2:
+            return url.backUrl.character
+        default:
+            return url.backUrl.organization_tree
+    }
+}
+
+const getMenuDtoItem = (menuId: string, onChange: (str: string) => void): MenuProps['items'] => {
+    switch (parseInt(menuId)) {
+        case 1:
+            return [
+                {
+                    key: 1,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.organization_tree)
+                            }}
+                            color='#ffffff'
+                        >
+                            组织结构树
+                        </Button>
+                    )
+                }, {
+                    key: 2,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.section)
+                            }}
+                            color='#ffffff'
+                        >
+                            分部列表
+                        </Button>
+                    )
+                }, {
+                    key: 3,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.depart)
+                            }}
+                            color='#ffffff'
+                        >
+                            部门列表
+                        </Button>
+                    )
+                }, {
+                    key: 4,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.section)
+                            }}
+                            color='#ffffff'
+                        >
+                            人员列表
+                        </Button>
+                    )
+                },
+            ]
+        case 2:
+            return [
+                {
+                    key: 1,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.character)
+                            }}
+                            color='#ffffff'
+                        >
+                            角色列表
+                        </Button>
+                    )
+                }
+            ]
+        case 3:
+            return [
+                {
+                    key: 1,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.module)
+                            }}
+                            color='#ffffff'
+                        >
+                            应用列表
+                        </Button>
+                    )
+                }, {
+                    key: 2,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.table)
+                            }}
+                            color='#ffffff'
+                        >
+                            表单列表
+                        </Button>
+                    )
+                }, {
+                    key: 3,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.column)
+                            }}
+                            color='#ffffff'
+                        >
+                            字段列表
+                        </Button>
+                    )
+                }
+            ]
+        case 4:
+            return [
+                {
+                    key: 1,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.content)
+                            }}
+                            color='#ffffff'
+                        >
+                            目录列表
+                        </Button>
+                    )
+                },
+            ]
+        case 5:
+            return [
+                {
+                    key: 1,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.workflow)
+                            }}
+                            color='#ffffff'
+                        >
+                            流程列表
+                        </Button>
+                    )
+                }, {
+                    key: 1,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.workflow_route)
+                            }}
+                            color='#ffffff'
+                        >
+                            路径列表
+                        </Button>
+                    )
+                }, {
+                    key: 1,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.workflow_node)
+                            }}
+                            color='#ffffff'
+                        >
+                            节点列表
+                        </Button>
+                    )
+                }, {
+                    key: 1,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.request)
+                            }}
+                            color='#ffffff'
+                        >
+                            流程监控
+                        </Button>
+                    )
+                },
+            ]
+        case 6:
+            return [
+                {
+                    key: 1,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.search_list)
+                            }}
+                            color='#ffffff'
+                        >
+                            展示列表
+                        </Button>
+                    )
+                }, {
+                    key: 2,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.search_list_column)
+                            }}
+                            color='#ffffff'
+                        >
+                            展示字段列表
+                        </Button>
+                    )
+                },
+            ]
+        case 7:
+            return [
+                {
+                    key: 1,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.menu)
+                            }}
+                            color='#ffffff'
+                        >
+                            页面菜单
+                        </Button>
+                    )
+                }, {
+                    key: 1,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.login_config)
+                            }}
+                            color='#ffffff'
+                        >
+                            登录设置
+                        </Button>
+                    )
+                }, {
+                    key: 1,
+                    label: (
+                        <Button
+                            type='link'
+                            onClick={() => {
+                                onChange(url.backUrl.page_config)
+                            }}
+                            color='#ffffff'
+                        >
+                            页面设置
+                        </Button>
+                    )
+                },
+            ]
+    }
+}
+
+const HumanAvater = (prop: { humanSelf: HumanOut, humanPhoto: FileOut | undefined }) => {
+    let avater
+    if (prop.humanPhoto !== undefined)
+        avater = <Avatar shape="square" size={16} src={config.backUrl + prop.humanPhoto.fileRoute} />
+    else
+        avater = <Avatar shape="square" size={16} icon={<UserOutlined />} />
+
+    return (
+        <Space align='center' style={{ display: "flex" }} size='middle'>
+            <Divider type="vertical" style={{ color: "#d9d9d9" }} />
+            <Dropdown
+                menu={{
+                    items: [
+                        {
+                            key: 'self',
+                            label: (
+                                <a href={url.frontUrl.humanResource + prop.humanSelf.dataId}>
+                                    <UserOutlined />
+                                    个人信息
+                                </a>
+                            )
+                        }, {
+                            key: 'jump',
+                            label: (
+                                <a href={url.front}>
+                                    <RetweetOutlined />
+                                    前台用户中心
+                                </a>
+                            )
+                        }, {
+                            key: 'exit',
+                            label: (
+                                <a href={url.login}>
+                                    <PoweroffOutlined />
+                                    退出登录
+                                </a>
+                            )
+                        }
+                    ],
+                }} >
+                <Space align='center' style={{ display: "flex" }} size='small'>
+                    {avater}
+                    <Title level={4} style={{ color: "#ffffff" }}>{prop.humanSelf.name}</Title>
+                </Space>
+            </Dropdown>
+        </Space>
+    )
+}
+
+const BackPage: React.FC = () => {
+    const menuId = new URLSearchParams(window.location.search.substring(1)).get("menuId")
+    const [humanSelf, setHumanSelf] = useState<HumanOut>()
+    const [humanPhoto, setHumanPhoto] = useState<FileOut>()
+    const [pageConfig, setPageConfig] = useState<PageConfig>()
+    const [aimUrl, setAimUrl] = useState<string>(getFirstPage(menuId))
+    useEffect(() => {
+        if (pageConfig === undefined)
+            getDataOne(config.fronts.page_config).then((value) => {
+                if (value.success)
+                    setPageConfig(value.data)
+                else
+                    setPageConfig({
+                        companyName: "EOA",
+                        headerColor: "#FFC89A",
+                        sideColor: "#ABA09F"
+                    })
+
+            })
+    })
+    useEffect(() => {
+        if (humanSelf === undefined) {
+            getDataOne(config.fronts.human_self).then((value) => {
+                if (value.success)
+                    setHumanSelf(value.data)
+                if (value.data.photo !== undefined && value.data.photo != null && value.data.photo !== 0)
+                    getDataOne(config.fronts.file + "/" + value.data.photo).then((photo) => {
+                        if (photo.success)
+                            setHumanPhoto(photo.data)
+                    })
+            })
+        }
+    })
+
+
+    if (pageConfig === undefined || humanSelf == undefined)
+        return <PageWait />
+
+    return (
+        <Layout>
+            <Header style={{ display: 'flex', alignItems: 'center' }} hasSider>
+                <Space align='center' style={{ backgroundColor: pageConfig?.headerColor, display: 'flex' }}>
+                    <div style={{ width: "200" }}><Title level={3} color='#ffffff'></Title></div>
+                    <BankTwoTone href={""} />
+                    <Dropdown
+                        menu={{ items: getRootMenuItem() }}>
+                        <Space>
+                            <AppstoreTwoTone />
+                            引擎
+                        </Space>
+                    </Dropdown>
+                    <Space />
+                    <Sider width={200}>
+                        <HumanAvater humanSelf={humanSelf} humanPhoto={humanPhoto} />
+                    </Sider>
+                </Space>
+            </Header>
+            <Layout>
+                <Sider width={200} style={{ background: pageConfig.sideColor }}>
+                    <Menu
+                        mode="inline"
+                        defaultSelectedKeys={['1']}
+                        defaultOpenKeys={['sub1']}
+                        style={{ height: '100%', borderRight: 0 }}
+                        items={getMenuDtoItem(menuId ?? "1", (str: string) => { if (str) setAimUrl(str) })}
+                    />
+                </Sider>
+                <Content>
+                    <iframe height="99%" width="99%" src={aimUrl} style={{ border: "none" }} />
+                </Content>
+            </Layout>
+        </Layout>
+    );
+};
+
+export default BackPage;
--- src/route/fornt.tsx
+++ src/route/fornt.tsx
@@ -13,7 +13,8 @@ import {
     TabletFilled,
     PoweroffOutlined,
     RetweetOutlined,
-    UserOutlined
+    UserOutlined,
+    BankTwoTone
 } from '@ant-design/icons';
 import type { ProSettings } from '@ant-design/pro-components';
 import {
@@ -45,149 +46,27 @@ type PageConfig = {
     sideColor: string
 }
 
-const defaultProps = {
-    route: {
-        path: '/',
-        routes: [
-            {
-                path: '/welcome',
-                name: '欢迎',
-                icon: <SmileFilled />,
-                component: './Welcome',
-            },
-            {
-                path: '/admin',
-                name: '管理页',
-                icon: <CrownFilled />,
-                access: 'canAdmin',
-                component: './Admin',
-                routes: [
-                    {
-                        path: '/admin/sub-page1',
-                        name: '一级页面',
-                        icon: 'https://gw.alipayobjects.com/zos/antfincdn/upvrAjAPQX/Logo_Tech%252520UI.svg',
-                        component: './Welcome',
-                    },
-                    {
-                        path: '/admin/sub-page2',
-                        name: '二级页面',
-                        icon: <CrownFilled />,
-                        component: './Welcome',
-                    },
-                    {
-                        path: '/admin/sub-page3',
-                        name: '三级页面',
-                        icon: <CrownFilled />,
-                        component: './Welcome',
-                    },
-                ],
-            },
-            {
-                name: '列表页',
-                icon: <TabletFilled />,
-                path: '/list',
-                component: './ListTableList',
-                routes: [
-                    {
-                        path: '/list/sub-page',
-                        name: '列表页面',
-                        icon: <CrownFilled />,
-                        component:'/login',
-                        routes: [
-                            {
-                                path: 'sub-sub-page1',
-                                name: '一一级列表页面',
-                                icon: <CrownFilled />,
-                                component: './Welcome',
-                            },
-                            {
-                                path: 'sub-sub-page2',
-                                name: '一二级列表页面',
-                                icon: <CrownFilled />,
-                                component: './Welcome',
-                            },
-                            {
-                                path: 'sub-sub-page3',
-                                name: '一三级列表页面',
-                                icon: <CrownFilled />,
-                                component: './Welcome',
-                            },
-                        ],
-                    },
-                    {
-                        path: '/list/sub-page2',
-                        name: '二级列表页面',
-                        icon: <CrownFilled />,
-                        component: './Welcome',
-                    },
-                    {
-                        path: '/list/sub-page3',
-                        name: '三级列表页面',
-                        icon: <CrownFilled />,
-                        component: './Welcome',
-                    },
-                ],
-            },
+const getRootMenuItem = (menus: MenuOut[]): { icon:string, title:string, desc:string, url:string }[] => {
+    return menus.map((menu) => {
+        return {
+            icon:"https://img0.baidu.com/it/u=3620010257,3904026948&fm=253&fmt=auto&app=138&f=JPEG?w=500&h=557",
+            title: menu.contentName,
+            desc:"",
+            url:url.front + "?menuId=" + menu.dataId
+        }
+    })
+}
 
-        ],
-    },
-    location: {
-        pathname: '/',
-    },
-    appList: [
-        {
-            icon: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
-            title: 'Ant Design',
-            desc: '杭州市较知名的 UI 设计语言',
-            url: 'https://ant.design',
-        },
-        {
-            icon: 'https://gw.alipayobjects.com/zos/antfincdn/FLrTNDvlna/antv.png',
-            title: 'AntV',
-            desc: '蚂蚁集团全新一代数据可视化解决方案',
-            url: 'https://antv.vision/',
-            target: '_blank',
-        },
-        {
-            icon: 'https://gw.alipayobjects.com/zos/antfincdn/upvrAjAPQX/Logo_Tech%252520UI.svg',
-            title: 'Pro Components',
-            desc: '专业级 UI 组件库',
-            url: 'https://procomponents.ant.design/',
-        },
-        {
-            icon: 'https://img.alicdn.com/tfs/TB1zomHwxv1gK0jSZFFXXb0sXXa-200-200.png',
-            title: 'umi',
-            desc: '插件化的企业级前端应用框架。',
-            url: 'https://umijs.org/zh-CN/docs',
-        },
-
-        {
-            icon: 'https://gw.alipayobjects.com/zos/bmw-prod/8a74c1d3-16f3-4719-be63-15e467a68a24/km0cv8vn_w500_h500.png',
-            title: 'qiankun',
-            desc: '可能是你见过最完善的微前端解决方案🧐',
-            url: 'https://qiankun.umijs.org/',
-        },
-        {
-            icon: 'https://gw.alipayobjects.com/zos/rmsportal/XuVpGqBFxXplzvLjJBZB.svg',
-            title: '语雀',
-            desc: '知识创作与分享工具',
-            url: 'https://www.yuque.com/',
-        },
-        {
-            icon: 'https://gw.alipayobjects.com/zos/rmsportal/LFooOLwmxGLsltmUjTAP.svg',
-            title: 'Kitchen ',
-            desc: 'Sketch 工具集',
-            url: 'https://kitchen.alipay.com/',
-        },
-        {
-            icon: 'https://gw.alipayobjects.com/zos/bmw-prod/d3e3eb39-1cd7-4aa5-827c-877deced6b7e/lalxt4g3_w256_h256.png',
-            title: 'dumi',
-            desc: '为组件开发场景而生的文档工具',
-            url: 'https://d.umijs.org/zh-CN',
-        },
-    ],
-};
-
+const getMenuDtoItem = (menuDto: MenuOut ) => {
+    return menuDto.children.map((menu) => {
+        return {
+            name: menu.contentName,
+            path: menu.contentUrl,
+            component: menu.contentUrl,
+            routes: getMenuDtoItem(menu)
+        }
+    })
+}
 
 export default () => {
     const [settings, setSetting] = useState<Partial<ProSettings> | undefined>({
@@ -257,12 +136,34 @@ export default () => {
         }
     })
 
+    
+
     if (pageConfig === undefined || pageConfig === null || menuRoots === undefined || menuDto === undefined || humanSelf == undefined)
         return <PageWait />
 
     if (typeof document === 'undefined') {
-        return <div />;
+        return <PageWait />;
     }
+
+    const defaultProps = {
+        route: {
+            path: '/',
+            routes: [
+                {
+                    path: menuDto?.contentUrl,
+                    name: menuDto?.contentName,
+                    icon: <CrownFilled />,
+                    access: menuDto?.contentUrl,
+                    component: menuDto?.contentUrl,
+                    routes: getMenuDtoItem(menuDto)
+                },
+            ],
+        },
+        location: {
+            pathname: '/',
+        },
+        appList: getRootMenuItem(menuRoots)
+    };
     return (
         <div
             id="test-pro-layout"
@@ -313,26 +214,6 @@ export default () => {
                                 colorTextMenuActive: 'rgba(255,255,255,0.95)',
                             },
                         }}
-                        bgLayoutImgList={[
-                            {
-                                src: 'https://img.alicdn.com/imgextra/i2/O1CN01O4etvp1DvpFLKfuWq_!!6000000000279-2-tps-609-606.png',
-                                left: 85,
-                                bottom: 100,
-                                height: '303px',
-                            },
-                            {
-                                src: 'https://img.alicdn.com/imgextra/i2/O1CN01O4etvp1DvpFLKfuWq_!!6000000000279-2-tps-609-606.png',
-                                bottom: -68,
-                                right: -45,
-                                height: '303px',
-                            },
-                            {
-                                src: 'https://img.alicdn.com/imgextra/i3/O1CN018NxReL1shX85Yz6Cx_!!6000000005798-2-tps-884-496.png',
-                                bottom: 0,
-                                left: 0,
-                                width: '331px',
-                            },
-                        ]}
                         {...defaultProps}
                         location={{
                             pathname,
@@ -344,7 +225,6 @@ export default () => {
                         headerTitleRender={
                             (logo, title, _) =>  (
                                   <a href='/'>
-                                    {logo}
                                     {title}
                                   </a>
                                 )
@@ -352,7 +232,7 @@ export default () => {
                         avatarProps={{
                             src: humanPhoto?.fileRoute ?? 'https://gw.alipayobjects.com/zos/antfincdn/efFD%24IOql2/weixintupian_20170331104822.jpg',
                             size: 'small',
-                            title: '七妮妮',
+                            title: humanSelf.name,
                             render: (props, dom) => {
                                 return (
                                     <Dropdown
